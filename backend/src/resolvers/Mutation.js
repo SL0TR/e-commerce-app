0c752d10b8d0407895df150b35f83c49bcdba2aa
SL0TR <mohaiminislam94@gmail.com>
2019-05-16 01:20:37 +0600

    add delete query schema and mutation on backend

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -24,6 +24,18 @@ const Mutation = {
         id: args.id
       }
     }, info);
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where =  { id: args.id };
+
+    //find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+
+    //check if the own that item or have the permission
+    // TODO
+
+    // delete it.
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
