1ce221a68ad5c69b6e977a58ae37900969b62808
SL0TR <mohaiminislam94@gmail.com>
2019-05-18 13:06:17 +0600

    get the userId from cookie, verify it with jwt and send user data to frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,30 @@
-const cookieeParser = require('cookie-parser');
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
-
+const cookieeParser = require("cookie-parser");
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const jwt = require("jsonwebtoken");
+// const db = require("./db");
 
 const server = createServer();
 
 server.express.use(cookieeParser());
-// TODO USE express middleware to populate current user
+
+//decode the JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    //put the user id on the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL,
-    },
+      origin: process.env.FRONTEND_URL
+    }
   },
   deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,9 +1,21 @@
-const { forwardTo } = require('prisma-binding');
- 
+const { forwardTo } = require("prisma-binding");
+
 const Query = {
-  items: forwardTo('db'),
-  item: forwardTo('db'),
-  itemsConnection: forwardTo('db'),
+  items: forwardTo("db"),
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db"),
+  me(parent, args, ctx, info) {
+    //check if there's a current userId
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
