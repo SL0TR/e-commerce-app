f5dd72c1de69b2c88f968168be13bc49a7e828b1
SL0TR <mohaiminislam94@gmail.com>
2019-05-25 00:18:33 +0600

    add and configure react-alert plugin

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,15 @@
+import { transitions, positions } from "react-alert";
+
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
 export const perPage = 6;
+
+// optional cofiguration
+export const options = {
+  // you can also just use 'bottom center'
+  position: positions.TOP_CENTER,
+  timeout: 5000,
+  offset: "30px",
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+};
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,31 +1,36 @@
 import App, { Container } from "next/app";
-import Page from "../components/Page";
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 import { ApolloProvider } from "react-apollo";
 import withApollo from "../lib/withData";
+import Page from "../components/Page";
+import { options } from "../config";
 
 class MyApp extends App {
-  static async getInitialProps ({ Component, ctx }) {
-    let pageProps = {}
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
 
     if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
+      pageProps = await Component.getInitialProps(ctx);
     }
 
     // this exposes the query to the user
     pageProps.query = ctx.query;
 
-    return { pageProps }
+    return { pageProps };
   }
-  
+
   render() {
     const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
         <ApolloProvider client={apollo}>
-          <Page>
-            <Component {...pageProps} />
-          </Page>
+          <AlertProvider template={AlertTemplate} {...options}>
+            <Page>
+              <Component {...pageProps} />
+            </Page>
+          </AlertProvider>
         </ApolloProvider>
       </Container>
     );
